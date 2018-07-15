0753375cc5aaed67a07ceef82a8bc1d076ea3988
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2018-07-15 18:02:54 -0400

    Consulta TerMinada

--- RegistrodeLibrosBiblicos/UI/Consultas/ConsultadeLibrosBiblicos.Designer.cs
+++ RegistrodeLibrosBiblicos/UI/Consultas/ConsultadeLibrosBiblicos.Designer.cs
@@ -28,19 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.DesdedateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.HastadateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FiltrocomboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CriteriotextBox = new System.Windows.Forms.TextBox();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.ConsultadataGridView = new System.Windows.Forms.DataGridView();
+            this.Imprimirbutton = new System.Windows.Forms.Button();
+            this.FechacheckBox = new System.Windows.Forms.CheckBox();
+            this.GeneralerrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GeneralerrorProvider)).BeginInit();
             this.SuspendLayout();
+            // 
+            // DesdedateTimePicker
+            // 
+            this.DesdedateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DesdedateTimePicker.Location = new System.Drawing.Point(70, 57);
+            this.DesdedateTimePicker.Name = "DesdedateTimePicker";
+            this.DesdedateTimePicker.Size = new System.Drawing.Size(99, 20);
+            this.DesdedateTimePicker.TabIndex = 0;
+            // 
+            // HastadateTimePicker
+            // 
+            this.HastadateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.HastadateTimePicker.Location = new System.Drawing.Point(240, 57);
+            this.HastadateTimePicker.Name = "HastadateTimePicker";
+            this.HastadateTimePicker.Size = new System.Drawing.Size(102, 20);
+            this.HastadateTimePicker.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Desde :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hasta :";
+            // 
+            // FiltrocomboBox
+            // 
+            this.FiltrocomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FiltrocomboBox.FormattingEnabled = true;
+            this.FiltrocomboBox.Items.AddRange(new object[] {
+            "ID",
+            "DESCRIPCION",
+            "SIGLAS",
+            "TIPO"});
+            this.FiltrocomboBox.Location = new System.Drawing.Point(59, 9);
+            this.FiltrocomboBox.Name = "FiltrocomboBox";
+            this.FiltrocomboBox.Size = new System.Drawing.Size(121, 21);
+            this.FiltrocomboBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Filtro :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Criterio :";
+            // 
+            // CriteriotextBox
+            // 
+            this.CriteriotextBox.Location = new System.Drawing.Point(255, 12);
+            this.CriteriotextBox.Name = "CriteriotextBox";
+            this.CriteriotextBox.Size = new System.Drawing.Size(189, 20);
+            this.CriteriotextBox.TabIndex = 7;
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Location = new System.Drawing.Point(461, 12);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Buscarbutton.TabIndex = 8;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
+            // 
+            // ConsultadataGridView
+            // 
+            this.ConsultadataGridView.AllowUserToAddRows = false;
+            this.ConsultadataGridView.AllowUserToDeleteRows = false;
+            this.ConsultadataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ConsultadataGridView.Location = new System.Drawing.Point(21, 83);
+            this.ConsultadataGridView.Name = "ConsultadataGridView";
+            this.ConsultadataGridView.ReadOnly = true;
+            this.ConsultadataGridView.Size = new System.Drawing.Size(423, 262);
+            this.ConsultadataGridView.TabIndex = 9;
+            // 
+            // Imprimirbutton
+            // 
+            this.Imprimirbutton.Location = new System.Drawing.Point(21, 351);
+            this.Imprimirbutton.Name = "Imprimirbutton";
+            this.Imprimirbutton.Size = new System.Drawing.Size(75, 23);
+            this.Imprimirbutton.TabIndex = 10;
+            this.Imprimirbutton.Text = "Imprimir";
+            this.Imprimirbutton.UseVisualStyleBackColor = true;
+            // 
+            // FechacheckBox
+            // 
+            this.FechacheckBox.AutoSize = true;
+            this.FechacheckBox.Checked = true;
+            this.FechacheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.FechacheckBox.Location = new System.Drawing.Point(59, 34);
+            this.FechacheckBox.Name = "FechacheckBox";
+            this.FechacheckBox.Size = new System.Drawing.Size(56, 17);
+            this.FechacheckBox.TabIndex = 11;
+            this.FechacheckBox.Text = "Fecha";
+            this.FechacheckBox.UseVisualStyleBackColor = true;
+            // 
+            // GeneralerrorProvider
+            // 
+            this.GeneralerrorProvider.ContainerControl = this;
             // 
             // ConsultadeLibrosBiblicos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 450);
+            this.ClientSize = new System.Drawing.Size(547, 382);
+            this.Controls.Add(this.FechacheckBox);
+            this.Controls.Add(this.Imprimirbutton);
+            this.Controls.Add(this.ConsultadataGridView);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.CriteriotextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FiltrocomboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HastadateTimePicker);
+            this.Controls.Add(this.DesdedateTimePicker);
             this.Name = "ConsultadeLibrosBiblicos";
             this.Text = "Consulta de Libros Biblicos";
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GeneralerrorProvider)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker DesdedateTimePicker;
+        private System.Windows.Forms.DateTimePicker HastadateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox FiltrocomboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox CriteriotextBox;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.DataGridView ConsultadataGridView;
+        private System.Windows.Forms.Button Imprimirbutton;
+        private System.Windows.Forms.CheckBox FechacheckBox;
+        private System.Windows.Forms.ErrorProvider GeneralerrorProvider;
     }
 }
--- RegistrodeLibrosBiblicos/UI/Consultas/ConsultadeLibrosBiblicos.cs
+++ RegistrodeLibrosBiblicos/UI/Consultas/ConsultadeLibrosBiblicos.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using RegistrodeLibrosBiblicos.ENTIDADES;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +16,159 @@ namespace RegistrodeLibrosBiblicos.UI.Consultas
         public ConsultadeLibrosBiblicos()
         {
             InitializeComponent();
+        }
+
+
+
+        private void Buscarbutton_Click(object sender, EventArgs e)
+        {
+            Expression<Func<LibrosBiblicos, bool>> filtro = x => true;
+
+            switch (FiltrocomboBox.SelectedIndex)
+            {
+                case 0://Id
+
+                    if (Validar(1))
+                    {
+                        MessageBox.Show("Favor Llenar Casilla ", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (Validar(2))
+                    {
+                        MessageBox.Show("Debe Digitar un Numero!", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        int id = Convert.ToInt32(CriteriotextBox.Text);
+
+                        filtro = x => x.LibroId == id;
+
+                        if (BLL.LibrosBiblicosBLL.GetList(filtro).Count() == 0)
+                        {
+                            MessageBox.Show("Este ID, No Existe", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return;
+                        }
+                    }
+
+
+                    break;
+
+                case 1://Descripcion
+
+                    if (Validar(1))
+                    {
+                        MessageBox.Show("Favor Llenar Casilla ", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (Validar(3))
+                    {
+                        MessageBox.Show("Debe Digitar una Descripcion!", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        filtro = x => x.Descripcion.Contains(CriteriotextBox.Text);
+
+                        if (BLL.LibrosBiblicosBLL.GetList(filtro).Count() == 0)
+                        {
+                            MessageBox.Show("Esta Descripcion, No Existe", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        }
+                    }
+                    break;
+
+                case 2://Siglas
+
+                    if (Validar(1))
+                    {
+                        MessageBox.Show("Favor Llenar Casilla ", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (Validar(3))
+                    {
+                        MessageBox.Show("Debe Digitar Caracteres!", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        
+                        filtro = x => x.Siglas.Equals(CriteriotextBox.Text);
+
+
+                        if (BLL.LibrosBiblicosBLL.GetList(filtro).Count() == 0)
+                        {
+                            MessageBox.Show("Este Monto, No Existe", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        }
+                    }
+                    break;
+
+                case 3://Tipo
+
+                    if (Validar(1))
+                    {
+                        MessageBox.Show("Favor Llenar Casilla ", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (Validar(3))
+                    {
+                        MessageBox.Show("Debe Digitar Caracteres!", "Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                     
+                        filtro = x => x.Tipo.Equals(CriteriotextBox.Text);
+
+
+                        if (BLL.LibrosBiblicosBLL.GetList(filtro).Count() == 0)
+                        {
+                            MessageBox.Show("Este Monto, No Existe", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        }
+                    }
+
+
+
+                    break;
+
+                case 4://TODO
+
+                    filtro = x => true;
+                    break;
+
+            }
+            ConsultadataGridView.DataSource = BLL.LibrosBiblicosBLL.GetList(filtro);
+            CriteriotextBox.Clear();
+            GeneralerrorProvider.Clear();
+
+
+        }
+
+        private bool Validar(int error)
+        {
+            bool paso = false;
+            int num = 0;
+
+            if (error == 1 && string.IsNullOrEmpty(CriteriotextBox.Text))
+            {
+                GeneralerrorProvider.SetError(CriteriotextBox, "Por Favor, LLenar Casilla!");
+                paso = true;
+            }
+            if (error == 2 && int.TryParse(CriteriotextBox.Text, out num) == false)
+            {
+                GeneralerrorProvider.SetError(CriteriotextBox, "Debe Digitar un Numero");
+                paso = true;
+            }
+
+            if (error == 3 && int.TryParse(CriteriotextBox.Text, out num) == true)
+            {
+                GeneralerrorProvider.SetError(CriteriotextBox, "Debe Digitar Caracteres");
+                paso = true;
+            }
+
+            return paso;
         }
     }
 }
